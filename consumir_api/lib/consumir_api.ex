be1c51aa9c5aa37be7e960e5e4a184fc75b868aa
Selene Flores <xochitl.selene.fp@gmail.com>
2021-04-05 10:44:37 -0500

    elixir project created

---
+++ consumir_api/lib/consumir_api.ex
@@ -0,0 +1,18 @@
+defmodule ConsumirApi do
+  @moduledoc """
+  Documentation for ConsumirApi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ConsumirApi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
